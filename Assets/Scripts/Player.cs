40188e3ad86aafe7829dac6e4e19a4d477fc7e05
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-24 13:48:37 -0300

    added a ViewPortToWorldPoint() clamp

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,10 +5,29 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 10f;
+    [SerializeField] [Range(0 ,1)] float limitMinX = 0f;
+    [SerializeField] [Range(0, 1)] float limitMaxX = 1f;
+    [SerializeField] [Range(0, 1)] float limitMinY = 0f;
+    [SerializeField] [Range(0, 1)] float limitMaxY = 1f;
+
+    float xMin;
+    float xMax;
+    float yMin;
+    float yMax;
 
     void Start()
     {
-        
+        SetUpMoveBoundaries();
+    }
+
+    private void SetUpMoveBoundaries()
+    {
+        Camera gameCamera = Camera.main;
+        xMin = gameCamera.ViewportToWorldPoint(new Vector3(limitMinX, 0, 0)).x;
+        xMax = gameCamera.ViewportToWorldPoint(new Vector3(limitMaxX, 0, 0)).x;
+        yMin = gameCamera.ViewportToWorldPoint(new Vector3(0, limitMinY, 0)).y;
+        yMax = gameCamera.ViewportToWorldPoint(new Vector3(0, limitMaxY, 0)).y;
+
     }
 
     void Update()
@@ -19,9 +38,9 @@ public class Player : MonoBehaviour
     private void Move()
     {
         var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-        var newXPos = Mathf.Clamp(transform.position.x + deltaX, -15f, -9f);
         var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-        var newYPos = Mathf.Clamp(transform.position.y + deltaY, -7.5f, 7.5f);
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, xMin, xMax);
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, yMin, yMax);
         transform.position = new Vector2(newXPos, newYPos);
     }
 }
