e53399d9301ff7896ab241a615e07795597e5fe6
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-24 16:59:51 -0300

    Enemies moves in a path

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class EnemyPathing : MonoBehaviour
+{
+    [SerializeField] List<Transform> waypoints;
+    [SerializeField] float moveSpeed = 2f;
+
+    int waypointIndex = 0;
+
+    void Start()
+    {
+        transform.position = waypoints[waypointIndex].position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= waypoints.Count - 1)
+        {
+            var targetPosition = waypoints[waypointIndex];
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition.position, movementThisFrame);
+            if (transform.position == targetPosition.position)
+            {
+                waypointIndex++;
+            }
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
