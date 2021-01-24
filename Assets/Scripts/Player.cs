9671c1ae2871c0c4498e128751ba38636717aefd
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-24 12:14:57 -0300

    added sprites and player basic movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 10f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    private void Move()
+    {
+        var deltaX = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        var newXPos = Mathf.Clamp(transform.position.x + deltaX, -15f, -9f);
+        var deltaY = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        var newYPos = Mathf.Clamp(transform.position.y + deltaY, -7.5f, 7.5f);
+        transform.position = new Vector2(newXPos, newYPos);
+    }
+}
