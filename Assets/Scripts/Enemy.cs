bd03a4dbb5dc5da605f638e9cd61a22ac77054cb
Felipe Camêlo <felipecamelobrx@gmail.com>
2021-01-25 08:23:08 -0300

    now the enemies reckonize damage

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] int health = 1000;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        DamageDealer damageDealer = collision.gameObject.GetComponent<DamageDealer>();
+        health = health - damageDealer.GetDamage();
+        damageDealer.Hit();
+    }
+}
